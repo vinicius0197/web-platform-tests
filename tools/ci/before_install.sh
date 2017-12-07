1d44d51524c3abb29032af8a31739e1fa650c8a2
Geoffrey Sneddon <me@gsnedders.com>
2017-12-07 16:28:12 +0000

    Remove duplicate copies of subdomains/hostnames

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-set -e
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start 1>&2
 
-if [[ $(./wpt test-jobs --includes $JOB; echo $?) -eq 0 ]]; then
-    export RUN_JOB=1
-    git submodule update --init --recursive 1>&2
-    export DISPLAY=:99.0
-    sh -e /etc/init.d/xvfb start 1>&2
-    # For uploading the manifest
-    export WPT_MANIFEST_FILE=$HOME/meta/MANIFEST-$(git rev-parse HEAD).json
-else
-    export RUN_JOB=0
-fi
